b675b5e6ca1c5eb8275f3ee7e70da3efaab3bec0
polanty <Tijaniabiola1599@gmail.com>
2025-01-23 16:39:32 -0800

    started adding my big image pictures

--- src/sections/BigImagesLink/BigImagesLink.jsx
+++ src/sections/BigImagesLink/BigImagesLink.jsx
@@ -1,20 +1,29 @@
 import "../BigImagesLink/BigImagesLink.css";
+import image3 from "../../../src/assets/courasel/image3.png";
+import HomePage_advert from "../../../src/assets/courasel/HomePage_advert.webp";
 
 const BigImageLink = () => {
   return (
     <>
       <div className="BigImageLinkContainer">
         <div className="BigImageLinkContainer_advert">
-          <h1>ADVERTISEMENT</h1>
+          <img
+            src={HomePage_advert}
+            className="card-image--banner"
+            alt="Banner"
+          />
         </div>
 
         <div className="BigImagesLinkContainer_links">
-          <h1 className="BigImageLinkContainer_link">
-            The BigImageLink Container
-          </h1>
-          <h1 className="BigImageLinkContainer_link">
-            The BigImageLink Container
-          </h1>
+          <img
+            src="https://i8.amplience.net/i/jpl/jd_704612_a?qlt=92&…oimage-lrg.png%202x%22%20style=%22height:%20auto;"
+            className="card-image--banner"
+            alt="Banner"
+          />
+          {/* <h1 className="BigImageLinkContainer_link">
+            The BigImageLink Container C:\Users\USER\Downloads\sandbox\jDsportsClone\src\assets\courasel\image3.png
+          </h1> */}
+          <img src={image3} className="card-image--banner" alt="Banner" />
         </div>
       </div>
     </>
