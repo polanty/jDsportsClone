39018fea4f9afed977b1d658f65491b8bb27c4eb
polanty <Tijaniabiola1599@gmail.com>
2025-04-28 01:40:48 -0700

    Fixed recently viewed container

--- src/components/Recenetly-Viewed/Recently-Viewed.components.jsx
+++ src/components/Recenetly-Viewed/Recently-Viewed.components.jsx
@@ -1,20 +1,19 @@
-import "../../components/Recenetly-Viewed/Recently-Viewed.css";
+// import "../../components/Recenetly-Viewed/Recently-Viewed.css";
+import "../Recenetly-Viewed/Recentlyview2.css";
 import Product from "../IndividualProduct/Product.components";
 
 const RecentlyViewedContainer = ({ recentlViewedContainer }) => {
   return (
-    <div className="ProductView__recentlyViewed">
+    <div className="brands__container">
       <div className="ProductView__recentlyViewed-innerContainer">
         <h3 className="footer-link__header">Recently Viewed</h3>
-        <div className="recently__viewed-item-container">
+        <div className="product__container">
           {recentlViewedContainer
-            .filter((_, ind) => ind < 5)
+            .filter((_, ind) => ind < 4)
             .map((product, ind) => (
-              <Product
-                product={product}
-                key={ind}
-                classNAME={`recentlyViewed__product`}
-              />
+              <div key={ind} className="TopPicksCategory__container">
+                <Product product={product} />
+              </div>
             ))}
         </div>
       </div>
